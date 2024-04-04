d4ec76fb709f87cf80fea713d1ce5c3b6571178c
hatim ourrach <ourrachhatim0@gmail.com>
2024-04-04 20:59:53 +0000

    i guess it works

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,40 +1,43 @@
-import Book from '../models/Book.js'
-import {writeBook, readBooks} from "../utils/fsHelper.js"
+import Book from "../models/Book.js";
+import { writeBook, readBooks } from "../utils/fsHelper.js";
 
 const allBooks = (req, res) => {
   readBooks((books) => {
-    res.render("index", {books})
-  })
-}
+    res.render("index", { books });
+  });
+};
 
-const getBook = (req,res) => {
-  readBooks((books) => {
-    const book = books.find(e => e.id == req.params.id)
-    res.render("book", {book})
-  })
-}
-
-const addBook = (req,res) => {
-
+const getBook = (req, res) => {
   readBooks((books) => {
-    const new_id = books[books.length - 1].id + 1
-    const new_data = new Book(new_id,  "Hyperion",  "Dan Simmons", 1992)
-    books.push(Object.assign({}, new_data))
-    writeBook(books, ()=>{
-      console.log("Book is added ! ")
-      res.render("index", {books})
-    })
-  })
+    const book = books.find((e) => e.id == req.params.id);
+    res.render("book", { book });
+  });
+};
 
-}
+const addBook = (req, res) => {
+  readBooks((books) => {
+    let new_id = 1;
+    if (books.length >= 1) {
+      new_id = books[books.length - 1].id + 1;
+    }
+    const { title, author, year } = req.body;
+    const new_data = new Book(new_id, title, author, year);
+    console.log(new_data);
+    books.push(Object.assign({}, new_data));
+    writeBook(books, () => {
+      console.log("Book is added ! ");
+      res.render("index", { books });
+    });
+  });
+};
 
-const deleteBook = (req,res) => {
-  readBooks((books)=> {
-    const deletedBooks = books.filter(e => e.id != req.params.id)
-    writeBook(deletedBooks, ()=>{
-      res.render("index", {books})
-    })
-  })
-}
+const deleteBook = (req, res) => {
+  readBooks((books) => {
+    const deletedBooks = books.filter((e) => e.id != req.params.id);
+    writeBook(deletedBooks, () => {
+      res.redirect("/books");
+    });
+  });
+};
 
-export {allBooks, getBook, addBook , deleteBook}
+export { allBooks, getBook, addBook, deleteBook };
--- routers/router.js
+++ routers/router.js
@@ -1,11 +1,15 @@
-import express from "express"
-import {allBooks, getBook, addBook, deleteBook} from "../controllers/bookController.js"
-const router = express.Router()
+import express from "express";
+import {
+  allBooks,
+  getBook,
+  addBook,
+  deleteBook,
+} from "../controllers/bookController.js";
+const router = express.Router();
 
+router.get("/", allBooks); //All Books
+router.get("/:id", getBook); // Get ONE book
+router.post("/", addBook); // Add one book
+router.post("/:id", deleteBook); // Delete one book
 
-router.get('/', allBooks ) //All Books
-router.get('/:id', getBook) // Get ONE book
-router.post('/', addBook) // Add one book
-router.post('/:id', deleteBook) // Delete one book
-
-export default router
+export default router;
